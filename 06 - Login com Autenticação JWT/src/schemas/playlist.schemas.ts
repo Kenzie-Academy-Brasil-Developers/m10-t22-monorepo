b6a234530f8ae6af471ca19252d72b653036966a
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-08-02 15:46:24 -0300

    added m10-t22-demo6 e 7

---
+++ 06 - Login com Autenticação JWT/src/schemas/playlist.schemas.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const playlistSchema = z.object({
+  id: z.number().int().positive(),
+  title: z.string().max(50),
+  userId: z.number().int().positive(),
+});
+
+export const playlistCreateSchema = playlistSchema.omit({ id: true });
